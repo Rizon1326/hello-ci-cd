5234f3987eef6471988f4c69b7609a01b21b4087
Rizon1326 <bsse1326@iit.du.ac.bd>
2025-01-16 09:12:04 +0600

    test case

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, it is my 1st CI/CD project!');
-  res.send('Hello, it is my 1st CI/CD project! and the change number is 1');
+  res.send('Hello, it is my 1st CI/CD project! and the change number is 2');
+});
+app.get('/', (req, res) => {
+  res.send('Hello, it is my test case for CI/CD project');
 });
 
 app.listen(port, () => {
